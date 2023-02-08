dde65333927a3d5e5a43bcbddfd23edc531e472c
rahulkumar22 <rahul.kumar059805@gmail.com>
2023-02-09 01:52:18 +0530

    BasketAPI

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class BasketController:ControllerBase
+    {
+        private readonly IBasketReopository _basketReopository;
+
+        public BasketController(IBasketReopository basketReopository)
+        {
+            _basketReopository = basketReopository;
+        }
+        [HttpGet("{userName}",Name ="GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket=await _basketReopository.GetBasket(userName);
+            return Ok(basket?? new ShoppingCart(userName));
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart shoppingCart)
+        {
+            return Ok(await _basketReopository.UpdateBasket(shoppingCart));
+        }
+
+        [HttpDelete("{userName}",Name ="DeleteBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketReopository.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repository/IBasketReopository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repository
+{
+    public interface IBasketReopository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+
+    }
+}
